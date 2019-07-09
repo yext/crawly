bfec02106453b17434618a0513ba3b3d63771fde
jexu <jexu@yext.com>
2019-07-09 16:56:44 -0400

    try catch

--- puppet.js
+++ puppet.js
@@ -1,8 +1,8 @@
 // import * from './Analytics.js';
-import {SelectorTracking} from './Helpers.js';
-import {GetParams} from './Helpers.js';
-import {CheckAnchorQueries} from './Helpers.js';
-import {SearchElementForSelector} from './Helpers.js';
+// import {SelectorTracking} from './Helpers.js';
+// import {GetParams} from './Helpers.js';
+// import {CheckAnchorQueries} from './Helpers.js';
+// import {SearchElementForSelector} from './Helpers.js';
 // import {CalcEventNameForElement} from './Helpers.js';
 // import {CalcEventNameMap} from './Helpers.js';
 import { Warn } from './Utils.js';
@@ -39,10 +39,11 @@ rl.question(`Enter  URL: `, (url) => {
       try {
           var browser = await puppeteer.launch({
             headless: false,
-            slowMo: 200,
-            defaultViewport: null
+            defaultViewport: null,
+            devtools: true
           });
           var page = await browser.newPage();
+          // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
 
 // TODO: find out how to append username and password into url????
@@ -57,6 +58,14 @@ rl.question(`Enter  URL: `, (url) => {
             jsonUrl = jsonUrl + "/index.json";
           }
           else if(url.endsWith(".com/"))
+          {
+            jsonUrl = jsonUrl + "index.json";
+          }
+          else if(url.endsWith(".ca"))
+          {
+            jsonUrl = jsonUrl + "/index.json";
+          }
+          else if(url.endsWith(".ca/"))
           {
             jsonUrl = jsonUrl + "index.json";
           }
@@ -105,8 +114,15 @@ rl.question(`Enter  URL: `, (url) => {
             }
             return jsonData;
           });
+          //
+          // await page.setViewport({
+          //     width: 100,
+          //     height: 100
+          // });
 
           await page.goto(`${url}`);
+
+
 
           await page.exposeFunction('CalcEventNameForElement', (name, referrer, url) => {
             let type = null;
@@ -137,6 +153,7 @@ rl.question(`Enter  URL: `, (url) => {
               }
               return null;
             }
+
             function GetParams(url) {
               let queries = {};
               let parts = url.split('?');
@@ -150,6 +167,9 @@ rl.question(`Enter  URL: `, (url) => {
             }
             function CheckAnchorQueries(anchor) {
               if (anchor && anchor.href) {
+                // console.log("askdjfasd");
+                // console.log(anchor);
+                // console.log(anchor.href);
                 let eName = GetParams(anchor.href)['ya-track'];
                 if (eName) {
                   return eName;
@@ -162,104 +182,110 @@ rl.question(`Enter  URL: `, (url) => {
             var events = document.querySelectorAll('button, a, input');
 
             events.forEach(function(element){
-              let type = null;
-              let trackDetails = null;
-              let srcEl = null;
+              try {
+                let type = null;
+                let trackDetails = null;
+                let srcEl = null;
 
-              for (const selector in SelectorTracking) {
-                if (!element.matches(selector)) continue;
-                trackDetails = SelectorTracking[selector];
-              }
-
-              if (!trackDetails) {
-                let potentialYaTrackedEl = SearchElementForSelector(element, '[data-ya-track]');
-                if (potentialYaTrackedEl) {
-                  srcEl = potentialYaTrackedEl;
-                  trackDetails = (potentialYaTrackedEl.dataset ? potentialYaTrackedEl.dataset.yaTrack : potentialYaTrackedEl.getAttribute('data-ya-track'));
+                for (const selector in SelectorTracking) {
+                  if (!element.matches(selector)) continue;
+                  trackDetails = SelectorTracking[selector];
                 }
-              }
 
-              let preventDefaultEvent = SearchElementForSelector(element, '[data-ya-prevent-default]');
-
-              if (!preventDefaultEvent && !trackDetails) {
-                let anchor = SearchElementForSelector(element, 'a');
-                if (anchor) {
-                  srcEl = anchor;
-                  let anchorQuery = CheckAnchorQueries(anchor);
-                  if (anchorQuery) trackDetails = anchorQuery;
-                  if (!anchorQuery && !trackDetails) {
-                    type = 'link';
+                if (!trackDetails) {
+                  let potentialYaTrackedEl = SearchElementForSelector(element, '[data-ya-track]');
+                  if (potentialYaTrackedEl) {
+                    srcEl = potentialYaTrackedEl;
+                    trackDetails = (potentialYaTrackedEl.dataset ? potentialYaTrackedEl.dataset.yaTrack : potentialYaTrackedEl.getAttribute('data-ya-track'));
                   }
                 }
-              }
 
-              if (!preventDefaultEvent && !trackDetails && !type) {
-                let button = SearchElementForSelector(element, 'button');
-                if (button) {
-                  srcEl = button;
-                  type = 'button';
-                }
-              }
+                let preventDefaultEvent = SearchElementForSelector(element, '[data-ya-prevent-default]');
 
-              if (!preventDefaultEvent && !trackDetails && !type) {
-                let input = SearchElementForSelector(element, 'input');
-                if (input && input.type != 'hidden') {
-                  srcEl = input;
-                  type = 'input';
-                }
-              }
-
-              let dataYaTrack = type || trackDetails;
-              let name = null;
-
-              if(dataYaTrack)
-              {
-                let scopeAncestors = [];
-                while (element && element.tagName) {
-                  if (element.matches('[data-ya-scope]')) {
-                    scopeAncestors.push(element);
+                if (!preventDefaultEvent && !trackDetails) {
+                  let anchor = SearchElementForSelector(element, 'a');
+                  if (anchor) {
+                    srcEl = anchor;
+                    let anchorQuery = CheckAnchorQueries(anchor);
+                    if (anchorQuery) trackDetails = anchorQuery;
+                    if (!anchorQuery && !trackDetails) {
+                      type = 'link';
+                    }
                   }
-                  element = element.parentNode;
                 }
 
-                let tags = [srcEl].concat(scopeAncestors);
-                  for (const [hierarchyIdx, hierarchyElement] of tags.entries()) {
-                    let tagVal = (hierarchyIdx == 0) ? dataYaTrack : (hierarchyElement.dataset ? hierarchyElement.dataset.yaScope : hierarchyElement.getAttribute('data-ya-scope'))
-                    if (tagVal.indexOf('#') > -1) {
-                      let attributeName = hierarchyIdx == 0 ? 'data-ya-track': 'data-ya-scope';
-                      let ancestor = (hierarchyIdx + 1 < tags.length) ? tags[hierarchyIdx + 1]: document;
-                      let siblings = Array.from(ancestor.querySelectorAll(`[${attributeName}='${tagVal}']`));
-                      for (const [siblingIdx, sibling] of siblings.entries()) {
-                        if (hierarchyElement == sibling) {
-                          tagVal = tagVal.replace('#', siblingIdx + 1);
-                          break;
+                if (!preventDefaultEvent && !trackDetails && !type) {
+                  let button = SearchElementForSelector(element, 'button');
+                  if (button) {
+                    srcEl = button;
+                    type = 'button';
+                  }
+                }
+
+                if (!preventDefaultEvent && !trackDetails && !type) {
+                  let input = SearchElementForSelector(element, 'input');
+                  if (input && input.type != 'hidden') {
+                    srcEl = input;
+                    type = 'input';
+                  }
+                }
+
+                let dataYaTrack = type || trackDetails;
+                let name = null;
+
+                if(dataYaTrack)
+                {
+                  let scopeAncestors = [];
+                  while (element && element.tagName) {
+                    if (element.matches('[data-ya-scope]')) {
+                      scopeAncestors.push(element);
+                    }
+                    element = element.parentNode;
+                  }
+
+                  let tags = [srcEl].concat(scopeAncestors);
+                    for (const [hierarchyIdx, hierarchyElement] of tags.entries()) {
+                      let tagVal = (hierarchyIdx == 0) ? dataYaTrack : (hierarchyElement.dataset ? hierarchyElement.dataset.yaScope : hierarchyElement.getAttribute('data-ya-scope'))
+                      if (tagVal.indexOf('#') > -1) {
+                        let attributeName = hierarchyIdx == 0 ? 'data-ya-track': 'data-ya-scope';
+                        let ancestor = (hierarchyIdx + 1 < tags.length) ? tags[hierarchyIdx + 1]: document;
+                        let siblings = Array.from(ancestor.querySelectorAll(`[${attributeName}='${tagVal}']`));
+                        for (const [siblingIdx, sibling] of siblings.entries()) {
+                          if (hierarchyElement == sibling) {
+                            tagVal = tagVal.replace('#', siblingIdx + 1);
+                            break;
+                          }
                         }
                       }
+                      tags[hierarchyIdx] = tagVal;
                     }
-                    tags[hierarchyIdx] = tagVal;
-                  }
-                  let name = tags.reverse().join('_');
-                  names.push(name);
+                    let name = tags.reverse().join('_');
+                    names.push(name);
+                }
+              } catch (err){
+                console.log(err);
               }
-            });
-            names.forEach(function(name){
-              var url =  window.CalcEventNameForElement(name, pagesReferrer, pageurl);
-              url.then(data => {
-                const px = document.createElement("img");
-                px.src = data;
-                px.style.width = '0';
-                px.style.height = '0';
-                px.style.position = 'absolute';
-                px.alt = '';
-                document.body.appendChild(px);
               });
-            });
+              names.forEach(function(name){
+                var url = window.CalcEventNameForElement(name, pagesReferrer, pageurl);
+                url.then(data => {
+                  const px = document.createElement("img");
+                  px.src = data;
+                  px.style.width = '0';
+                  px.style.height = '0';
+                  px.style.position = 'absolute';
+                  px.alt = '';
+                  document.body.appendChild(px);
+                  return px;
+                });
+              });
+
           });
           await page.waitFor(100000);
           await browser.close();
           console.log(success("Browser Closed"));
       } catch (err) {
-          console.log(error(err));
+          console.log(err);
           await browser.close();
           console.log(error("Browser Closed"));
       }
